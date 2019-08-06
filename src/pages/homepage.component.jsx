5c4c5ac5db843788632879b77b887301a9354050
Temi Makinde <Temi.Makinde@Gmail.com>
2019-08-06 15:28:25 +0100

    added shop page, collection preview and collection item pages

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Directory from "../../src/components/directory/directory.component";
+import Directory from "../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <h1>Welcome to Moxi Clothings</h1>
+    <h1>Moxi Clothings</h1>
     <div className="directory-menu">
       <Directory />
     </div>
