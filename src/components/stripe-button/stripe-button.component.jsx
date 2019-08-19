cf947e5d5bdf2c44338cfdc36103fcf5d66dac18
Temi Makinde <Temi.Makinde@Gmail.com>
2019-08-19 23:04:20 +0100

    stripes integration, advanced routing and many more on redux

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const onToken = token => {
+  console.log(token);
+  alert("Payment was successful");
+};
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_A3xeVYv1QYnMVXv9I31ePwKj00XacYj3hp";
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Moxi Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is £${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+      currency="GBP"
+    />
+  );
+};
+
+export default StripeCheckoutButton;
