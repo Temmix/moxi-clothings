6bdd47fdd4669fa97eadb350ae688e546db9e780
Temi Makinde <Temi.Makinde@Gmail.com>
2019-08-29 21:09:08 +0100

    Converted custom button and collection-item component to use Jsx css styles

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -55,7 +55,6 @@ export const CustomButtonContainer = styled.button`
   text-transform: uppercase;
   font-family: "Open Sans Condensed";
   font-weight: bolder;
-  border: none;
   cursor: pointer;
   display: flex;
   justify-content: center;
