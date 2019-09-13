5908de2edd7d2041342def36d22b0fe176ab8f54
Temi Makinde <Temi.Makinde@Gmail.com>
2019-09-13 15:37:27 +0100

    Back end implementation of stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
-const onToken = token => {
-  console.log(token);
-  alert("Payment was successful");
-};
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_A3xeVYv1QYnMVXv9I31ePwKj00XacYj3hp";
+
+  const onToken = token => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment error ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please ensure you use the provided credit card"
+        );
+      });
+  };
 
   return (
     <StripeCheckout
