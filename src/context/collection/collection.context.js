faecb6c4407d116685f1a24833f0ea68aa950302
Temi Makinde <Temi.Makinde@Gmail.com>
2019-09-17 10:45:22 +0100

    UsinguseContext of context api to access data on collection component

---
+++ src/context/collection/collection.context.js
@@ -0,0 +1,6 @@
+import { creatContext } from "react";
+import SHOP_DATA from "./shop.data";
+
+const CollectionsContext = creatContext(SHOP_DATA);
+
+export default CollectionsContext;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { connect } from "react-redux";
-
+import React, { useContext } from "react";
 import CollectionItem from "../../components/collection-item/collection-item.component";
-import { selectCollection } from "../../redux/shop/shop.selector";
+import CollectionsContext from "../../context/collection/collection.context";
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
 
   return (
@@ -20,8 +20,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
